2b4056f9223c7f27855ba5730107efa5e6519ae1
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-03-22 14:41:52 +0530

    Create Code : Triangular Star Pattern.java

---
+++ Lecture 5 : Patterns - 1/Code : Triangular Star Pattern/Code : Triangular Star Pattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i=0,j=0;
+		while(i<n){
+			while(j<=i){
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			j=0;
+			i++;
+		}
+		
+	}
+
+}
