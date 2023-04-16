2bf8e8ac87e70ddf092abc4c26a5dde875cb3b6c
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-04-16 23:48:21 +0530

    Create Code Merge Two Sorted Arrays.java

---
+++ Lecture 10 :  Arrays 2/Code Merge Two Sorted Arrays/Code Merge Two Sorted Arrays.java
@@ -0,0 +1,35 @@
+
+public class Solution {  
+
+    public static int[] merge(int arr1[], int arr2[]) {
+    	int m=arr1.length;
+    int n=arr2.length;
+    int[] arr=new int[m+n];
+    int i=0;
+    int j=0;
+    int k=0;
+    while(i<m && j<n){
+        if(arr1[i]<=arr2[j]){
+            arr[k]=arr1[i];
+            i++;
+            k++;
+        }else{
+            arr[k]=arr2[j];
+            j++;
+            k++;
+        }
+    }
+    while(i<m){
+        arr[k]=arr1[i];
+        i++;
+        k++;
+    }
+    while(j<n){
+        arr[k]=arr2[j];
+        j++;
+        k++;
+    }
+    return arr;
+    }
+
+}
