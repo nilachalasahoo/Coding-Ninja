83cabcf01637267bd51ad63946569cb5d29c5ebc
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-04-23 00:18:25 -0700

    Create Check Array Rotation.java

---
+++ Lecture 10 :  Arrays 2/Check Array Rotation/Check Array Rotation.java
@@ -0,0 +1,13 @@
+public class Solution {
+
+    public static int arrayRotateCheck(int[] arr){
+        int n=arr.length;
+    	for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1])
+                return i+1;
+                
+        }
+        return 0;
+    }
+
+}
