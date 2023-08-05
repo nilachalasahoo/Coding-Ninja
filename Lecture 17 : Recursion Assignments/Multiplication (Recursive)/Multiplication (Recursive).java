b108f7cfe516c579cc4f0fdaa0efa2c42367bc05
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-08-05 23:35:57 +0530

    Create Multiplication (Recursive).java

---
+++ Lecture 17 : Recursion Assignments/Multiplication (Recursive)/Multiplication (Recursive).java
@@ -0,0 +1,10 @@
+
+public class solution {
+
+	public static int multiplyTwoIntegers(int m, int n){
+		// Write your code here
+		if(m==0 || n==0)
+			return 0;
+		return m+multiplyTwoIntegers(m, n-1);
+	}
+}
