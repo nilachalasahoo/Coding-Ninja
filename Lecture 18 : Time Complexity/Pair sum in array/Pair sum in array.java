6e37a7caf55191ccb6a9420118ee59d928a038d0
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-08-16 23:45:24 +0530

    Update Pair sum in array.java

--- Lecture 18 : Time Complexity/Pair sum in array/Pair sum in array.java
+++ Lecture 18 : Time Complexity/Pair sum in array/Pair sum in array.java
@@ -4,13 +4,31 @@ public class Solution {
 	public static int pairSum(int[] arr, int num) {
 		//Your code goes here
 		Arrays.sort(arr);
-		int n=arr.length-1,s=0;
-		if(n<=0)
-			return 0;
-		s=arr[0]+arr[n];
-		n--;
-		for(int i=1;i<n;i++){
-			if(s==num)
+		int n=arr.length-1;
+		int i=0,c=0;
+		while(i<n){
+			if(arr[i]+arr[n]<num)
+				i++;
+			else if(arr[i]+arr[n]>num)
+				n--;
+			else{
+				if(arr[n]==arr[i]){
+					c+=(n-i)*(n-i+1)/2;
+					break;
+				}
+				else{
+					int st=i+1;
+					int end=n-1;
+					while(arr[i]==arr[st])
+						st++;
+					while(arr[n]==arr[end])
+						end--;
+					c+=(n-end)*(st-i);
+					i=st;
+					n=end;
+				}
+			}
 		}
+		return c;
 	}
 }
