fb81198d8430eb134b239983bc797f90d6083903
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-08-18 19:43:24 +0530

    Create Length Of LL.java

---
+++ Lecture 19 : Linked List - I/Length Of LL/Length Of LL.java
@@ -0,0 +1,40 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution {
+    public static int length(Node head){
+        //Your code goes here
+        int n=0;
+        while(head!=null){
+            head=head.next;
+            n++;
+        }
+        return n;
+    }
+}
