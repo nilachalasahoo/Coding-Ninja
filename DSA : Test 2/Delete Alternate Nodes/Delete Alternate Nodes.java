28dae754aa0537af55c0da236a26eec06e66a1f3
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-08-23 22:55:40 +0530

    Create Delete Alternate Nodes.java

---
+++ DSA : Test 2/Delete Alternate Nodes/Delete Alternate Nodes.java
@@ -0,0 +1,35 @@
+/* 
+
+    Following is the class used to represent the Node of a Singly Linked List
+
+    class Node<T> {
+        public T data;
+        public Node<T> next;
+
+        public Node(T data) {
+            this.setData(data);
+            this.next = null;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+    }
+*/
+
+public class Solution {
+
+    public static void deleteAlternateNodes(Node<Integer> head) {
+        //Your code goes here
+        Node<Integer> temp=head;
+        while(temp!=null && temp.next!=null){
+            temp.next=temp.next.next;
+            temp=temp.next;
+        }
+    }
+}
