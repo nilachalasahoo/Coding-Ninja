939c8a2c7bb000cc0fd2d25756f85905c222cf8f
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-08-24 23:05:29 +0530

    Create Sum Of Nodes.java

---
+++ Lecture 23 : Binary Trees - I/Sum Of Nodes/Sum Of Nodes.java
@@ -0,0 +1,28 @@
+/*
+
+	Following is the Binary Tree Node class structure.
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+ */
+
+public class Solution {
+
+	public static int getSum(BinaryTreeNode<Integer> root) {
+		//Your code goes here.
+		if(root==null)
+			return 0;
+		return root.data+getSum(root.left)+getSum(root.right);
+	}
+
+}
