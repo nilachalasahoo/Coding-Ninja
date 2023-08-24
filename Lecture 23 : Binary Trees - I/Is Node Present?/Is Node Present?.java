360d64218b819d83916152204947078d567944a9
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-08-24 22:32:47 +0530

    Create Is Node Present?.java

---
+++ Lecture 23 : Binary Trees - I/Is Node Present?/Is Node Present?.java
@@ -0,0 +1,30 @@
+/*
+	
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+*/
+
+public class Solution {
+
+	public static boolean isNodePresent(BinaryTreeNode<Integer> root, int x) {
+	    //Your code goes here
+		if(root==null)
+			return false;
+		if(root.data==x)
+			return true;
+		return isNodePresent(root.left, x)||isNodePresent(root.right, x);
+	}
+
+}
