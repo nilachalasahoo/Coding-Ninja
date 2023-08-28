e139defff21de8b5e21afeea08935a70d04c43d3
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-08-28 23:00:55 +0530

    Create Search In BST.java

---
+++ Lecture 25 : BST - I/Search In BST/Search In BST.java
@@ -0,0 +1,22 @@
+public class Solution {
+
+	/*
+	 * Binary Tree Node class
+	 * 
+	 * class BinaryTreeNode<T> { T data; BinaryTreeNode<T> left; BinaryTreeNode<T> right;
+	 * 
+	 * public BinaryTreeNode(T data) { this.data = data; } }
+	 */
+
+
+	public static boolean searchInBST(BinaryTreeNode<Integer> root, int k) {
+		if(root==null)
+			return false;
+		if(root.data==k)
+			return true;
+		if(root.data>k)
+			return searchInBST(root.left,k);
+		else
+			return searchInBST(root.right,k);
+	}
+}
